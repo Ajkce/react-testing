776804b122da06563fe919de28472ece5a2effe5
Ajaya Bahadur K C <kshettryaj93@gmail.com>
2023-09-24 00:17:31 +1000

    Testing db connection

--- data/database.js
+++ data/database.js
@@ -5,19 +5,18 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://ajaya:Ajayakc13101@cluster0.nba51dt.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp`;
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&appName=AtlasApp`;
 
 const client = new MongoClient(uri);
 
-console.log("Trying to connect to db w");
+console.log("Trying to connect to db");
 
 try {
   await client.connect();
-  console.log("connected");
   await client.db(dbName).command({ ping: 1 });
   console.log("Connected successfully to server");
 } catch (error) {
-  console.log("Connection failed connection failed.");
+  console.log("Connection failed.");
   await client.close();
   console.log("Connection closed.");
 }
