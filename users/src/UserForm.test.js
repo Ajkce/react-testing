0c6a7f018b5329e5f60687380c6b980360c845c9
Ajaya Bahadur K C <kshettryaj93@gmail.com>
2023-10-30 22:17:32 +1100

    Adding the user inputs when the form is submitted

--- users/src/UserForm.test.js
+++ users/src/UserForm.test.js
@@ -14,3 +14,31 @@ test("It shows two inputs and a button", () => {
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument();
 });
+
+test("It call the onuseradd when the function is sumbitted", () => {
+  //Callback
+  const argList = [];
+  const callback = (...args) => {
+    argList.push(args);
+  };
+
+  render(<UserForm onUserAdd={callback}></UserForm>);
+
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+
+  //Simulate Typeing inside the input
+
+  user.click(nameInput);
+  user.keyboard("jane");
+
+  user.click(emailInput);
+  user.keyboard("jane@jane.com");
+
+  //FInd the button
+  const button = screen.getByRole("button");
+  user.click(button);
+
+  //Assertion to make sure 'onUserAdd' gets called with email/name
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({ name: "jane", email: "jane@jane.com" });
+});
