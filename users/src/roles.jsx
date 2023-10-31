ec74781172c30e82254d7ba725bb82ca22939258
Ajaya Bahadur K C <kshettryaj93@gmail.com>
2023-10-31 15:32:08 +1100

    Adding rtl notebooke to check when to use differenet query functions

---
+++ users/src/roles.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const roles = () => {
+  return (
+    <div>
+      <a href="/">Link</a>
+      <button>Button</button>
+      <footer>COntentInfo</footer>
+      <h1>Heading</h1>
+      <header>Banner</header>
+      <img src="" alt="description" />
+      <input type="text" name="" id="" />
+      <input type="checkbox" name="" id="" />
+      <input type="radio" name="" id="" />
+      <input type="number" name="" id="" />
+      <li>ListItem</li>
+      <ul>List group</ul>
+    </div>
+  );
+};
+
+export default roles;
